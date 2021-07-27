522d73d21f1e9bc6bc3c297edc9ba24a3d373e4b
Aitor <aitor@telnyx.com>
2021-07-27 14:00:26 +0200

    Refactor several `_exec` methods parameters naming to improve readability (#89)
    This was triggered from this comment on previous PR: https://github.com/ansible-collections/community.rabbitmq/pull/65#discussion_r672111654

--- plugins/modules/rabbitmq_plugin.py
+++ plugins/modules/rabbitmq_plugin.py
@@ -111,8 +111,8 @@ class RabbitMqPlugins(object):
         else:
             self._rabbitmq_plugins = module.get_bin_path('rabbitmq-plugins', True)
 
-    def _exec(self, args, run_in_check_mode=False):
-        if not self.module.check_mode or (self.module.check_mode and run_in_check_mode):
+    def _exec(self, args, force_exec_in_check_mode=False):
+        if not self.module.check_mode or (self.module.check_mode and force_exec_in_check_mode):
             cmd = [self._rabbitmq_plugins]
             rc, out, err = self.module.run_command(cmd + args, check_rc=True)
             return out.splitlines()
