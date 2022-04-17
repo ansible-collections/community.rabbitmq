dbfca867bc3b1a320c0b06a5f62ed86a50671a21
Chris Smart <distroguy@gmail.com>
2022-04-17 17:38:54 +1000

    Use vendored LooseVersion and StrictVersion
    distutils has been deprecated and will be removed from Python's stdlib
    in Python 3.12 (see https://www.python.org/dev/peps/pep-0632/). This PR
    replaces the use of distutils.version.LooseVersion and
    distutils.version.StrictVersion with the vendored copy of
    distutils.version included with ansible-core
    2.12.

--- plugins/modules/rabbitmq_policy.py
+++ plugins/modules/rabbitmq_policy.py
@@ -83,7 +83,7 @@ EXAMPLES = r'''
 
 import json
 import re
-from distutils.version import LooseVersion as Version
+from ansible_collections.community.rabbitmq.plugins.module_utils.version import LooseVersion as Version
 
 from ansible.module_utils.basic import AnsibleModule
 
