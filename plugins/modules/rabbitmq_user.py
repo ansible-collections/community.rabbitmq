5a8abd06ba6102862bae9c4ae9b1629ccf4b847a
Chris Smart <distroguy@gmail.com>
2022-07-07 21:03:36 +1000

    user module: set supports_check_mode flag to False
    Currently, check_mode is not supported on the user module and as such, a
    user is actually modified when run with the `--check` option.
    
    This an initial temporary fix in order to avoid issues on existing
    systems.
    
    See #107

--- plugins/modules/rabbitmq_user.py
+++ plugins/modules/rabbitmq_user.py
@@ -465,7 +465,7 @@ def main():
     )
     module = AnsibleModule(
         argument_spec=arg_spec,
-        supports_check_mode=True
+        supports_check_mode=False
     )
 
     username = module.params['user']
